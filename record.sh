f2ecdffd668a37eadcdecde2b65f6d61646be43d
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2022-01-10 20:20:10 +0000

    Initial commit

---
+++ record.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Stream duration $@"
+
+duration=$1
+
+# Start ffmpeg
+ffmpeg -i https://abcradiolivehls-lh.akamaihd.net/i/triplejnsw_1@327300/master.m3u8 \
+  -nostdin \
+  -t $duration \
+  -map 0:a:3 \
+  -loglevel info \
+  /download/$EPOCHSECONDS.aac >> output.log 2>&1 < /dev/null
